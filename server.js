d20eb784086c345bbb1ec022b7892e66705e906b
satyam7803 <satyam7803@gmail.com>
2025-10-25 17:06:59 +0530

    new env

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect('mongodb://localhost/carbon_tracker', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
